dc94dc2d7f07a77ff5f9a116024a84ad7f1c12e7
nurmuh221 <nurmuhammad221@gmail.com>
2023-04-05 14:59:08 +0800

    Update InputOutput.java

--- InputOutput.java
+++ InputOutput.java
@@ -1,8 +1,11 @@
-public class java.until.Scanner{
+import java.until.Scanner;
+
+public class InputOutput{
  public static void main (String [] args){
     Scanner scanner = new Scanner(System.in);
     String nama;
     System.out.print("Masukkan nama anda : ")
     nama = scanner.nextLine();
+    System.out.println("Hai, "+nama);
  }
 }
